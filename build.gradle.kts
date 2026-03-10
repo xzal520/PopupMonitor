cdf2c6d42e5b44afc10178ac6bce8f1d84ca8f85
xzal520 <111861878+xzal520@users.noreply.github.com>
2026-03-10 19:00:20 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
+// 根目录构建脚本
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
